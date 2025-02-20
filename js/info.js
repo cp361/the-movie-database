45340dbfbad9b52cbf05f91a93ee2e99ca9b0306
Casper Pedersen <casp7541@outlook.dk>
2025-02-21 00:51:26 +0100

    removed api token

--- js/info.js
+++ js/info.js
@@ -2,7 +2,7 @@ export const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NDk5OTc5OGQ4NzQ3YzdkMmI1MzM5MWUyY2U2ZmRkMSIsIm5iZiI6MTc0MDA0NDc4My45OTgwMDAxLCJzdWIiOiI2N2I2ZjllZmIzZjhmYjI3YzQxYTAxZWMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.Sfq-H64cJ663eXKcT0jxAMWwxdDYJX_CrgTm7797YVk'
+      Authorization: 'Bearer //*api token*//'
     }
   };
 
